2001e60c6ca9a568d477f231de7eabedbbfa86f3
SouravDas25 <souravbumbadas25@gmail.com>
2024-10-22 11:41:12 +0800

    clearing sg laptop

--- deploy-aws.ps1
+++ deploy-aws.ps1
@@ -12,7 +12,7 @@ npm run package
 cd ..
 scp -i ".\keys\first-key-pair.pem" srv/dist/deploy.zip  ec2-user@ec2-54-206-82-215.ap-southeast-2.compute.amazonaws.com:/home/ec2-user
 
-ssh -i ".\keys\first-key-pair.pem" ec2-user@ec2-54-206-82-215.ap-southeast-2.compute.amazonaws.com
+ssh -i ".\aws\first-key-pair.pem" ec2-user@ec2-54-79-180-197.ap-southeast-2.compute.amazonaws.com
 
 # rm auth-app -r -f
 # unzip deploy.zip -d "auth-app"
