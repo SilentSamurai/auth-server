b238bf5f2151b649f0dbc7617610b3f616915341
Sourav <souravbumbadas25@gmail.com>
2025-03-22 23:47:07 +0000

    helm tested

--- helm/mongo/script.sh
+++ helm/mongo/script.sh
@@ -1,3 +1,3 @@
 
 
-helm install mongo bitnami/mongodb -f mongo\mongo.values.yaml -n mongo --create-namespace
+helm upgrade --install mongo bitnami/mongodb -f mongo\mongo.values.yaml -n mongo --create-namespace
